8357fecf818d03dda86b0b2600e6dda15289177e
Prevole <prevole@prevole.ch>
2016-05-12 13:31:48 +0200

    Updated

--- jenkins/scripts/databases.sh
+++ jenkins/scripts/databases.sh
@@ -3,7 +3,7 @@
 # Make sure PostgreSQL is running
 docker-compose -p $PROBEDOCK_ENV up --no-recreate -d db
 FIRST_CONTAINER=$(docker-compose -p $PROBEDOCK_ENV ps -q db | head -n 1)
-docker-compose -p $PROBEDOCK_ENV run waitdb
+docker-compose -p $PROBEDOCK_ENV --rm run waitdb
 #docker run --rm --net=${PROBEDOCK_ENV}_probenet --link $FIRST_CONTAINER:db aanand/wait
 
 # Make sure Redis is running
