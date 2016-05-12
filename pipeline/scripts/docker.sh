b47d0a8dbb3507ce29ab65245974ec76d21f249b
Prevole <prevole@prevole.ch>
2016-05-12 15:46:57 +0200

    Updated

--- pipeline/scripts/docker.sh
+++ pipeline/scripts/docker.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Docker environment setup"
-
-#export DOCKER_TLS_VERIFY="1"
-#export DOCKER_HOST="tcp://192.168.99.100:2376"
-#export DOCKER_CERT_PATH="/Users/Shared/Jenkins/.docker/machine/machines/default"
-#export DOCKER_MACHINE_NAME="default"
-
-echo "Docker ready"
-
+echo "Docker check"
 docker ps
--- pipeline/scripts/redis.sh
+++ pipeline/scripts/redis.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sure PostgreSQL is running
-docker-compose -p $PROBEDOCK_ENV up --no-recreate -d db
-docker-compose -p $PROBEDOCK_ENV run --rm waitdb
-
 # Make sure Redis is running
 docker-compose -p $PROBEDOCK_ENV up --no-recreate -d cache
 docker-compose -p $PROBEDOCK_ENV run --rm waitcache
