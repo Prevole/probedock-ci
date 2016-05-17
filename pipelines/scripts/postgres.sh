461288f5579a13020bf95762052fe6299b30a75e
Prevole <prevole@prevole.ch>
2016-05-17 13:48:03 +0200

    Update

--- pipelines/scripts/postgres.sh
+++ pipelines/scripts/postgres.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set +x
-
-echo $POSTGRES_PASSWORD
-
 # Make sure PostgreSQL is running
 docker-compose -p $PROBEDOCK_ENV up --no-recreate -d db
 docker-compose -p $PROBEDOCK_ENV run --rm waitdb
