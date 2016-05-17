ab777817bb51770971e2f08ece91033480d034ed
Prevole <prevole@prevole.ch>
2016-05-17 11:58:02 +0200

    Update

--- pipelines/firstDeploy.groovy
+++ pipelines/firstDeploy.groovy
@@ -44,12 +44,12 @@ node {
     /**
      * Define the password names
      */
-    def POSTGRESSQL_PASSWORD_NAME = env.PROBEDOCK_ENV = '-PostgreSQLRoot'
+    POSTGRESSQL_PASSWORD_NAME = env.PROBEDOCK_ENV = '-PostgreSQLRoot'
 
     /**
      * Define the password names in Docker Compose env vars
      */
-    def DOCKER_POSTGRESQL_PASSWORD = 'POSTGRES_PASSWORD'
+    DOCKER_POSTGRESQL_PASSWORD = 'POSTGRES_PASSWORD'
 
     sh "echo -n \$(date '+%Y_%m_%d_%H_%M_%S') > date"
 
