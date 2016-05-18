865b5b7752433ae0a33d7ac887f95d960f9ff4bf
Prevole <prevole@prevole.ch>
2016-05-18 19:58:08 +0200

    Update

--- pipelines/src/Backup.groovy
+++ pipelines/src/Backup.groovy
@@ -8,7 +8,7 @@ node {
     def Passwords = load 'pipelines/src/Passwords.groovy'
     def DisableJob = load 'pipelines/src/DisableJob.groovy'
 
-    println DisableJob
+    println DisableJob.disableJob
 
     stage 'Disable job'
     DisableJob.disableJob('Backup')
