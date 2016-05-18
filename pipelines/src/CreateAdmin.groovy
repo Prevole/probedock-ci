2329a2376c3a8bc7b7c5d32f5c89c90d297c48e7
Prevole <prevole@prevole.ch>
2016-05-18 13:22:28 +0200

    Update

---
+++ pipelines/src/CreateAdmin.groovy
@@ -0,0 +1,19 @@
+//noinspection GroovyAssignabilityCheck
+node {
+    env.PROBEDOCK_ENV = PROBEDOCK_ENV
+
+    // Clone the pipelines repos and the probe dock server repo
+    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'WipeWorkspace']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Prevole/probedock-ci']]]
+
+    def passwords = load 'pipelines/src/Passwords.groovy'
+
+    /**
+     * We want to create the admin user
+     */
+    stage 'Create the database'
+    withCredentials([
+        [$class: 'StringBinding', credentialsId: passwords.PROBEDOCK_DB_PASSWORD_NAME, variable: passwords.DOCKER_PROBEDOCK_DB_PASSWORD_VARNAME]
+    ]) {
+        sh 'pipelines/scripts/create-admin.sh'
+    }
+}
