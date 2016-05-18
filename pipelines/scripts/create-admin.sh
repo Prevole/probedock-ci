c0c5d3c14e6dee23f333312143b67abaf633f2da
Prevole <prevole@prevole.ch>
2016-05-18 12:51:58 +0200

    Update

--- pipelines/scripts/create-admin.sh
+++ pipelines/scripts/create-admin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'Create Probe Dock administrator: ${PROBEDOCK_ADMIN_USERNAME} with email ${PROBEDOCK_ADMIN_EMAIL}'
+echo "Create Probe Dock administrator: ${PROBEDOCK_ADMIN_USERNAME} with email ${PROBEDOCK_ADMIN_EMAIL}"
 docker-compose -p $PROBEDOCK_ENV run --rm --no-deps task \
     rake \
     "users:register[${PROBEDOCK_ADMIN_USERNAME},${PROBEDOCK_ADMIN_EMAIL},${PROBEDOCK_ADMIN_PASSWORD}]" \
