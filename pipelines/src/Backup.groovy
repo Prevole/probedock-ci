fc46985f09fa5251d5d266773eeab8be154c207b
Prevole <prevole@prevole.ch>
2016-05-18 20:28:18 +0200

    Update

--- pipelines/src/Backup.groovy
+++ pipelines/src/Backup.groovy
@@ -6,18 +6,14 @@ node {
     checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'WipeWorkspace']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Prevole/probedock-ci']]]
 
     def Passwords = load 'pipelines/src/Passwords.groovy'
-    def DisableJob = load 'pipelines/src/DisableJob.groovy'
 
-    stage 'Disable job'
-    DisableJob.disableJob('Backup')
-
-//    /**
-//     * Backup the PostgreSQL database
-//     */
-//    stage 'Backup the PostgresSQL database'
-//    withCredentials([
-//            [$class: 'StringBinding', credentialsId: Passwords.POSTGRESSQL_PASSWORD_NAME, variable: Passwords.DOCKER_POSTGRESQL_PASSWORD_VARNAME]
-//    ]) {
-//        sh 'pipeline/scripts/postgres-backup.sh'
-//    }
+    /**
+     * Backup the PostgreSQL database
+     */
+    stage 'Backup the PostgresSQL database'
+    withCredentials([
+            [$class: 'StringBinding', credentialsId: Passwords.POSTGRESSQL_PASSWORD_NAME, variable: Passwords.DOCKER_POSTGRESQL_PASSWORD_VARNAME]
+    ]) {
+        sh 'pipeline/scripts/postgres-backup.sh'
+    }
 }
