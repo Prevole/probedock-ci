a03efb3db8be6bd738c5e45c8667b86ff1771a8b
Prevole <prevole@prevole.ch>
2016-05-24 16:38:32 +0200

    Update

--- pipelines/src/FirstDeploy.groovy
+++ pipelines/src/FirstDeploy.groovy
@@ -1,7 +1,8 @@
 //noinspection GroovyAssignabilityCheck
 node {
     env.PROBEDOCK_ENV = PROBEDOCK_ENV
-    env.PROBEDOCK_DATA_PATH = PROBEDOCK_DATA_PATH
+
+    load('pipelines/src/LoadEnv.groovy').setupEnv(env, '/envs/' + env.PROBEDOCK_ENV)
 
     // Clone the pipelines repos and the probe dock server repo
     checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'WipeWorkspace']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Prevole/probedock-ci']]]
