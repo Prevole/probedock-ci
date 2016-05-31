3bb18a9b25317e70bdd25b472064896a6580b33a
Prevole <prevole@prevole.ch>
2016-05-31 10:23:33 +0200

    Update

--- pipelines/src/CreateOrUpdateEnvironment.groovy
+++ pipelines/src/CreateOrUpdateEnvironment.groovy
@@ -389,8 +389,7 @@ node {
     }
     else {
         println 'The environment configuration file was created.'
-        println env.FIRST_DEPLOY
-        if (env.FIRST_DEPLOY) {
+        if (Boolean.parseBoolean(env.FIRST_DEPLOY)) {
             println 'The first deploy will now be triggered.'
             build job: 'FirstDeploy', parameters: [
                 [$class: 'StringParameterValue', name: 'PROBEDOCK_ENV', value: env.PROBEDOCK_ENV],
