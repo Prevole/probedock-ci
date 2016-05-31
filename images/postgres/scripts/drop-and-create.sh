9e080e8d61105d4dbc371735e852b3bfa6f0b600
Prevole <prevole@prevole.ch>
2016-05-31 15:45:54 +0200

    Update

--- images/postgres/scripts/drop-and-create.sh
+++ images/postgres/scripts/drop-and-create.sh
@@ -2,7 +2,7 @@
 
 
 echo 'Drop the database'
-dropdb -h db -U "${POSTGRES_USER}" ${PROBEDOCK_DATABASE_NAME}
+dropdb -h db -U ${POSTGRES_USER} ${PROBEDOCK_DATABASE_NAME}
 
 echo 'Create the database'
-createdb -h db -U "${POSTGRES_USER}" -O ${PROBEDOCK_DATABASE_USERNAME} ${PROBEDOCK_DATABASE_NAME}
+createdb -h db -U ${POSTGRES_USER} -O ${PROBEDOCK_DATABASE_USERNAME} ${PROBEDOCK_DATABASE_NAME}
