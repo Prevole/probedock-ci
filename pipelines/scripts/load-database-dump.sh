09ec435383f2d580db175962bca46bea778c2050
Prevole <prevole@prevole.ch>
2016-05-31 15:52:07 +0200

    Update

--- pipelines/scripts/load-database-dump.sh
+++ pipelines/scripts/load-database-dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo 'Restore dump file'
-docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps db /scripts/load-dump.sh
+docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps loadDump /scripts/load-dump.sh
