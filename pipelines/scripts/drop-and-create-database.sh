6559506354c520e2ac2283b161d44eaf5b909f1c
Prevole <prevole@prevole.ch>
2016-05-31 15:30:41 +0200

    Update

--- pipelines/scripts/drop-and-create-database.sh
+++ pipelines/scripts/drop-and-create-database.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps loadDump echo $PROBEDOCK_DATABASE_NAME
+docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps loadDump echo ${PROBEDOCK_DATABASE_NAME}
+
 echo 'Drop the database'
 docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps loadDump dropdb -h db -U "$POSTGRES_USER" $PROBEDOCK_DATABASE_NAME
 
