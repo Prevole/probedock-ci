e13e4d835cbf9c2cc00968d963482e48c4a3fbf9
Prevole <prevole@prevole.ch>
2016-05-31 15:56:17 +0200

    Update

--- images/postgres/scripts/load-dump.sh
+++ images/postgres/scripts/load-dump.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Restore dump file'
+ls -laht /dumps
 psql -h db -U "${POSTGRES_USER}" -f ${DUMP_PATH} ${PROBEDOCK_DATABASE_NAME}
--- pipelines/scripts/load-database-dump.sh
+++ pipelines/scripts/load-database-dump.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-echo 'Restore dump file'
 docker-compose -f docker-compose-app.yml -p $PROBEDOCK_ENV run --rm --no-deps loadDump /scripts/load-dump.sh
