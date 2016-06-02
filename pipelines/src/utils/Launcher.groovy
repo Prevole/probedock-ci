3baa876cd4750620721914d5f71baa9137fe261e
Prevole <prevole@prevole.ch>
2016-06-02 17:34:37 +0200

    Update

--- pipelines/src/utils/Launcher.groovy
+++ pipelines/src/utils/Launcher.groovy
@@ -5,11 +5,12 @@ def launch(operation) {
 
     if (operation.equalsIgnoreCase('Update environment')) {
         jobName = 'CreateOrUpdateEnvironment'
+        probedockNewCloneRequired = false
     }
 
-//    if (probedockNewCloneRequired) {
-//        load('ci/pipelines/src/utils/Repos.groovy').cloneProbeDock('ci/images/probedock-base/probedock')
-//    }
+    if (probedockNewCloneRequired) {
+        load('ci/pipelines/src/utils/Repos.groovy').cloneProbeDock('ci/images/probedock-base/probedock')
+    }
 
     load('ci/pipelines/src/jobs/' + jobName + '.groovy').executeJob()
 }
