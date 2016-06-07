50b6e2783c2389339d268383ea495355e438a106
Prevole <prevole@prevole.ch>
2016-06-07 11:09:35 +0200

    Update

--- pipelines/scripts/nginx.sh
+++ pipelines/scripts/nginx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Make sure Nginx is running
-docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV up --no-recreate -d rp
+sudo docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV up --no-recreate -d rp
