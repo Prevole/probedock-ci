27afe24cad483e0ff357067d692abc308fd802ea
Prevole <prevole@prevole.ch>
2016-06-29 15:14:57 +0200

    Update

--- pipelines/scripts/gamedock-ssh-keys.sh
+++ pipelines/scripts/gamedock-ssh-keys.sh
@@ -11,8 +11,13 @@ for LINE in $GD_PRIVATE_KEY; do
   echo $LINE >> ~/.ssh/id_rsa
 done
 
-#echo $GD_PRIVATE_KEY > ~/.ssh/id_rsa
-#echo $GD_PUBLIC_KEY > ~/.ssh/id_rsa.pub
+touch ~/.ssh/id_rsa.pub
+
+IFS=$'\n'
+for LINE in $GD_PUBLIC_KEY; do
+  echo $LINE >> ~/.ssh/id_rsa.pub
+done
 
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
