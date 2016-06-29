6fd176a6550ca75fc96030f2b008ae2f55cddfc8
Prevole <prevole@prevole.ch>
2016-06-29 16:00:02 +0200

    Update

--- pipelines/scripts/viz-docker-images.sh
+++ pipelines/scripts/viz-docker-images.sh
@@ -5,8 +5,10 @@ cp ci/images/gamedock/docker-images/probedock-report-app/Dockerfile ci/images/ga
 
 echo 'Build the viz web image'
 cd ci/images/gamedock/probedock-report/client/app
+sed s/tmp\/app/./ Dockerfile > Dockerfile
 sudo -E docker build -t probedock/vizweb .
 
 echo 'Build the viz api image'
 cd ../../server
+sed s/tmp\/server/./ Dockerfile > Dockerfile
 sudo -E docker build -t probedock/vizapi .
