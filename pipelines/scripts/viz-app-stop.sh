3b0aeea5218807daad583574ab84e6b8eac38c24
Prevole <prevole@prevole.ch>
2016-06-29 16:42:03 +0200

    Update

--- pipelines/scripts/viz-app-stop.sh
+++ pipelines/scripts/viz-app-stop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Stop the Viz app containers
-sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV stop vizweb
-sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV stop vizapi
+sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV stop vizWeb
+sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV stop vizApi
 
 # Cleanup
-sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV rm -f vizweb
-sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV rm -f vizapi
+sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV rm -f vizWeb
+sudo -E docker-compose -f $PROBEDOCK_DOCKER_COMPOSE_FILE -p $PROBEDOCK_ENV rm -f vizApi
